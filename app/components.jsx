3d88bb874c070019fcab6087d48fc40cf15a043a
vyakulindaniel <vyakulindaniel@mail.ru>
2024-11-09 02:16:07 +0400

    Text(h1,h2,h3,p,sm)

--- app/components.jsx
+++ app/components.jsx
@@ -1,2 +1,30 @@
 import Link from 'next/link';
 import { IconCheck } from './icons';
+
+export function Text(props) {
+    let style = props.style;
+    let children = props.children
+
+    switch (style) {
+        case h1:
+            return (
+                <h1 className='h1'>{children}</h1>
+            )
+        case h2:
+            return (
+                <h2 className='h2'>{children}</h2>
+            )
+        case h3:
+            return (
+                <h3 className='h3'>{children}</h3>
+            )
+        case p:
+            return (
+                <p className='p'>{children}</p>
+            )
+        case sm:
+            return (
+                <span className='sm'>{children}</span>
+            )
+    }
+}
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,10 +1,18 @@
 'use client'
 import './style.css';
 
+import {
+   Text, 
+} from '../components';
+
 export default function Test() {
   return (
-    <div className='all'>
-      <h1 className="h1">Header1</h1>
+    <div className='elements'>
+      <Text style='h1'>Header1</Text>
+      <Text style='h2'>Header2</Text>
+      <Text style='h3'>Header3</Text>
+      <Text style='p'>Paragraph</Text>
+      <Text style='sm'>Small</Text>
     </div>
   );
 }
