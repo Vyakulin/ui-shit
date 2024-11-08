2204ccfcf62f29e37c9ac50364b5c64565cd2d45
vyakulindaniel <vyakulindaniel@mail.ru>
2024-11-09 02:18:58 +0400

    test-page layout

--- app/components.jsx
+++ app/components.jsx
@@ -6,23 +6,23 @@ export function Text(props) {
     let children = props.children
 
     switch (style) {
-        case h1:
+        case 'h1':
             return (
                 <h1 className='h1'>{children}</h1>
             )
-        case h2:
+        case 'h2':
             return (
                 <h2 className='h2'>{children}</h2>
             )
-        case h3:
+        case 'h3':
             return (
                 <h3 className='h3'>{children}</h3>
             )
-        case p:
+        case 'p':
             return (
                 <p className='p'>{children}</p>
             )
-        case sm:
+        case 'sm':
             return (
                 <span className='sm'>{children}</span>
             )
